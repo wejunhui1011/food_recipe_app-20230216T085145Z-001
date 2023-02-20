9484c37bae3bdbc6fc8b949f8834d087ab588f33
Your Name <you@example.com>
2023-02-21 02:12:10 +0700

    commit

--- food_recipe_app/VideoNo9Code/search.dart
+++ food_recipe_app/VideoNo9Code/search.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: must_be_immutable
+
 import 'dart:convert';
 import 'dart:developer';
 import 'package:flutter/material.dart';
@@ -44,7 +46,6 @@ class _SearchState extends State<Search> {
   }
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     getRecipes(widget.query);
   }
